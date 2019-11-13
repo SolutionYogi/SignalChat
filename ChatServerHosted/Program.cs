c88a66cae447553c7563a9239b234085e98a2212
Jd <divyesh.jariwala@adyne.com>
2019-11-13 18:04:55 -0500

    Prove out the concept of hosting SignalR in a service.

---
+++ ChatServerHosted/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using Autofac.Integration.SignalR;
+using ChatServerCS;
+using Microsoft.AspNet.SignalR;
+using Topshelf;
+
+namespace ChatServerHosted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rc = HostFactory.Run(x =>                                   //1
+            {
+                x.Service<TownCrier>(s =>                                   //2
+                {
+                    s.ConstructUsing(name=> new TownCrier());                //3
+                    s.WhenStarted(tc => tc.Start());                         //4
+                    s.WhenStopped(tc => tc.Stop());                          //5
+                });
+                x.RunAsLocalSystem();                                       //6
+
+                x.SetDescription("SignalR.ChatServer");                   //7
+                x.SetDisplayName("SignalR.ChatServer");                                  //8
+                x.SetServiceName("SignalR.ChatServer");                                  //9
+            });                                                             //10
+
+            var exitCode = (int) Convert.ChangeType(rc, rc.GetTypeCode());  //11
+            Environment.ExitCode = exitCode;
+        }
+    }
+
+    public static class App
+    {
+        private static IContainer _container;
+
+        public static void Initialize()
+        {
+            var builder = new ContainerBuilder();
+
+            // Register your SignalR hubs.
+            builder.RegisterHubs(Assembly.GetExecutingAssembly());
+
+            builder.RegisterType<ChatHub>().ExternallyOwned();
+
+            // Set the dependency resolver to be Autofac.
+            _container = builder.Build();
+            GlobalHost.DependencyResolver = new AutofacDependencyResolver(_container);
+        }
+    }
+}
---
+++ ChatServerHosted/TownCrier.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Timers;
+using Autofac;
+using Autofac.Integration.SignalR;
+using ChatServerCS;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Hosting;
+using Owin;
+
+namespace ChatServerHosted
+{
+    public class TownCrier
+    {
+        private readonly Timer _timer;
+
+        public TownCrier()
+        {
+            _timer = new Timer(1000) {AutoReset = true};
+            _timer.Elapsed += (sender, eventArgs) => Console.WriteLine("It is {0} and all is well", DateTime.Now);
+        }
+
+        public void Start()
+        {
+            _timer.Start();
+            
+
+            Task.Factory.StartNew(StartHub);
+
+        }
+
+        private void StartHub()
+        {
+            var url = "http://localhost:8080/";
+            using (WebApp.Start<Startup>(url))
+            {
+                Console.WriteLine($"Server running at {url}");
+                Console.ReadLine();
+            }
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+    }
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+
+            var builder = new ContainerBuilder();
+
+            // STANDARD SIGNALR SETUP:
+
+            // Get your HubConfiguration. In OWIN, you'll create one
+            // rather than using GlobalHost.
+            var config = new HubConfiguration();
+
+            // Register your SignalR hubs.
+            builder.RegisterHubs(Assembly.GetExecutingAssembly());
+            builder.RegisterType<ChatHub>().ExternallyOwned();
+
+            // Set the dependency resolver to be Autofac.
+            var container = builder.Build();
+            config.Resolver = new AutofacDependencyResolver(container);
+
+            // OWIN SIGNALR SETUP:
+
+            var hubConfig = new HubConfiguration();
+            app.MapSignalR("/signalchat", hubConfig);
+
+            GlobalHost.Configuration.MaxIncomingWebSocketMessageSize = null;
+        }
+    }
+}
